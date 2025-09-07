e3d179b1623558f429c1aa1009c72ecad617cc5a
Alexxus <oleoalexis@gmail.com>
2025-09-07 04:24:19 -0600

    cambios de pom para map structu

--- microServicio_Hoteleria_Restaurante/hoteleria/src/main/java/com/example/hoteleria/Calificacion/Infraestructura/Output/Mapper/CalificacionMapper.java
+++ microServicio_Hoteleria_Restaurante/hoteleria/src/main/java/com/example/hoteleria/Calificacion/Infraestructura/Output/Mapper/CalificacionMapper.java
@@ -3,6 +3,7 @@ package com.example.hoteleria.Calificacion.Infraestructura.Output.Mapper;
 import com.example.hoteleria.Calificacion.Dominio.Calificacion;
 import com.example.hoteleria.Calificacion.Infraestructura.Output.Entity.CalificacionEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -10,12 +11,25 @@ import java.util.List;
 
 public interface CalificacionMapper {
 
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "reservacion", source = "reservacion")
+    @Mapping(target = "comentario", source = "comentario")
+    @Mapping(target = "puntuacion", source = "puntuacion")
     Calificacion toCalificacion(CalificacionEntity calificacionEntity);
 
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "reservacion", source = "reservacion")
+    @Mapping(target = "comentario", source = "comentario")
+    @Mapping(target = "puntuacion", source = "puntuacion")
     CalificacionEntity toCalificacionEntity(Calificacion calificacion);
 
 
+
     List<Calificacion> toCalificacionList(List<CalificacionEntity> calificacionEntities);
+
+
 
     List<CalificacionEntity> toCalificacionEntityList(List<Calificacion> calificaciones);
 }
