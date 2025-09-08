c1da4c7fee60c4b8bb866fd7df9dae321336634b
Alexxus <oleoalexis@gmail.com>
2025-09-07 20:50:30 -0600

    ingreso de find client de eureka

--- microServicio_Hoteleria_Restaurante/Usuario/src/main/java/com/example/Usuario/UsuarioApplication.java
+++ microServicio_Hoteleria_Restaurante/Usuario/src/main/java/com/example/Usuario/UsuarioApplication.java
@@ -2,9 +2,9 @@ package com.example.Usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class UsuarioApplication {
 
 	public static void main(String[] args) {
